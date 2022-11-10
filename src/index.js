958d933847256c3ad023262dc257732788d7989a
Feras Jobeir <fjobeir@gmail.com>
2022-11-10 21:44:32 +0300

    Setup Styles

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import AuthProvider from './Components/AuthContext/AuthContext';
-
+import { BrowserRouter } from "react-router-dom"
+import './assets/css/bootstrap.min.css'
+import './assets/css/demo25.min.css'
+import './assets/vendor/fontawesome-free/css/all.min.css'
+import './assets/vendor/simple-line-icons/css/simple-line-icons.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </BrowserRouter>
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
