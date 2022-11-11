1435684ef73478f765060e889a75aeeeaaea49f5
Manalei97 <manal.ye.tr@gmail.com>
2022-11-11 15:04:36 +0300

    add sideBar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import Footer from "../Footer/Footer";
 import HeroSection from "../HeroSection/HeroSection";
 import Navbar from "../Navbar/Navbar";
 import SecondNavbar from "../SecondNavbar/SecondNavbar";
+import Sidebar from "../SideBar/SideBar";
 import Suggested from "../SuggestedCategories/Suggested";
 // import SingleBook from "../SingleBook/SingleBook";
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
        <Navbar />
        <HeroSection/>
        <SecondNavbar/>
+       <Sidebar/>
        <Suggested />
        <Footer />
        {/* <SingleBook/> */}
