73532bfbafa3bb8d677018337bb7802d983d2113
nhussein2026 <naser738nas@gmail.com>
2022-11-11 21:43:58 +0300

    add signup

--- src/screens/Signup/Signup.js
+++ src/screens/Signup/Signup.js
@@ -1,20 +1,21 @@
-import classes from './Signup.css'
-import logo from '../../assets/images/logo.png'
-import { useRef, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import './Signup.css';
+import logo from "../../assets/images/logo-with-safha.png"
+import {useRef} from 'react'
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
 const Signup = () => {
     const navigate = useNavigate()
-    const nameRef = useRef()
+    const usernameRef = useRef()
     const emailRef = useRef()
-    const passwordRef = useRef()
+    const passwordRef = useRef()      
     const passwordConfirmationRef = useRef()
-    const [loading, setLoading] = useState(false)
     const register = async () => {
-        setLoading(true)
-        const response = await fetch(`${process.env.REACT_APP_API}/users/register`, {
+        console.log(usernameRef.current.value)
+        const response = await fetch('http://ferasjobeir.com/api/users/register', {
             method: 'post',
             body: JSON.stringify({
-                name: nameRef.current.value,
+                name: usernameRef.current.value,
                 email: emailRef.current.value,
                 password: passwordRef.current.value,
                 password_confirmation: passwordConfirmationRef.current.value,
@@ -24,55 +25,68 @@ const Signup = () => {
             }
         })
         const json = await response.json()
-        setLoading(false)
-        window.alert(json.messages.join(', '))
-        if (json.success) {
-            navigate('/login')
+        window.alert(json.messages)
+        if (json.success){
+            navigate("/login")
         }
+        
+    }
+    const login = () => {
+        navigate('/login')
     }
     return (
-        <div className="container">
+        <>
+        <Navbar />
+        <div id='rang' className="container">
             <div className="row">
-                <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-                    <div className={`${classes.register} my-5 p-5`}>
-                        <div className={`${classes.logo} mb-4`}>
-                            <img src={logo} alt='' />
+                <div className="col-12 col-sm-8 offset-sm-2 col-md-4 offset-md-4 border border-ligh rounded-3">
+                        <a href='/'><img id='logo' src={logo} alt="Safha Logo" /></a>
+                    <h2 className="mb-3" style={{
+                            display: "flex",
+                            justifyContent: "center",
+                        }}>Create Account</h2>
+                    <form>
+                        <div className="mb-2">
+                            <label htmlFor="name" className="form-label">UserName</label>
+                            <input ref={usernameRef} placeholder="Enter Your username" type="text" className="form-control" id="name" />
                         </div>
-                        <h1 className={`${classes.title} mb-4`}>Create Account</h1>
-                        <div className='form-field mb-3'>
-                            <label htmlFor='name' className='mb-2'>Name</label>
-                            <input type='text' ref={nameRef} id="name" className='form-control' />
+                        <div className="mb-2">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input ref={emailRef} placeholder="Enter Your email" type="email" className="form-control" id="email" />
                         </div>
-                        <div className='form-field mb-3'>
-                            <label htmlFor='email' className='mb-2'>Email Address</label>
-                            <input type='email' ref={emailRef} id="email" className='form-control' />
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input ref={passwordRef} placeholder="Enter Your password" type="password" className="form-control" id="password" />
                         </div>
-                        <div className='form-field mb-3'>
-                            <label htmlFor='password' className='mb-2'>Password</label>
-                            <input type='password' ref={passwordRef} id="password" className='form-control' />
+                        <div className="mb-2">
+                            <label htmlFor="password" className="form-label">Password Confirmation</label>
+                            <input ref={passwordConfirmationRef} placeholder="Enter Your Password agian" type="password" className="form-control" id="password" />
                         </div>
-                        <div className='form-field mb-3'>
-                            <label htmlFor='password_confirmation' className='mb-2'>Password Confirmation</label>
-                            <input type='password' ref={passwordConfirmationRef} id="password_confirmation" className='form-control' />
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}>
+
+                        <button onClick={Signup}
+                            type="button"
+                            className="btn  mb-3" 
+                            id='signup-btn'
+                            >Sign up
+                        </button>
+                        <button onClick={Signup}
+                            type="button"
+                            className="btn  mb-3" 
+                            id='signup-btn'
+                            >Go to Lognin
+                        </button>
                         </div>
-                        <div className='row mt-5'>
-                            <div className='col-6'>
-                                <Link className='btn btn-dark w-100' to='/login'>Go To Login</Link>
-                            </div>
-                            <div className='col-6'>
-                                <button
-                                    className='btn btn-primary w-100'
-                                    disabled={loading}
-                                    onClick={register}>
-                                        {loading ? 'Please Wait' : 'Register'}
-                                    </button>
-                            </div>
-                        </div>
-                    </div>
+                    </form>
                 </div>
+
             </div>
         </div>
+        <Footer />
+        </>
     )
 }
-
-export default Signup
+export default Signup;
