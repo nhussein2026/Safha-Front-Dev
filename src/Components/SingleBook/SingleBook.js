6535d08fd7d49612200ea0f84955b387735e68e3
Manalei97 <manal.ye.tr@gmail.com>
2022-11-11 14:38:09 +0300

    Edit footer in singleBook components

--- src/Components/SingleBook/SingleBook.js
+++ src/Components/SingleBook/SingleBook.js
@@ -1,3 +1,5 @@
+import Footer from "../Footer/Footer";
+
 const SingleBook = () => {
 
     return (
@@ -268,7 +270,8 @@ const SingleBook = () => {
                                             </div>{/* End .add-product-review */}
                                         </div>{/* End .product-reviews-content */}
                                     </div>{/* End .tab-pane */}
-                                </div>{/* End .tab-content */}
+                                </div>
+                                
                             </div>{/* End .product-single-tabs */}
                             {/* End .product-single-tabs */}
                         </div>{/* End .col-lg-9 */}
@@ -672,88 +675,8 @@ const SingleBook = () => {
                 </div>
             </div>
             {/* End .main */}
-            <footer className="footer font2">
-                <div className="container">
-                    <div className="footer-middle">
-                        <div className="row">
-                            <div className="col-lg-5 mb-1 mb-lg-0">
-                                <a href="demo25.html"><img src="assets/images/logo-black.png" alt="Logo" width={111} height={44} className="m-b-4" /></a>
-                                <div className="contact-widget">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar feugiat
-                                        quam,
-                                        vitae aliquam urna egestas nec. Phasellus sit amet consequat tortor.</p>
-                                    <div className="row ls-0">
-                                        <div className="col-md-2-5">
-                                            <h6 className="text-uppercase mb-0">Questions?</h6>
-                                            <h3 className="text-primary m-b-4">(123) 456-7890</h3>
-                                        </div>
-                                        <div className="col-md-3-5">
-                                            <h6 className="text-uppercase mb-0">Payment Methods</h6>
-                                            <img src="assets/images/demoes/demo25/payment.jpg" alt="payment methods" className="footer-payments m-b-4" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>{/* End .col-lg-5 */}
-                            <div className="col-lg-2 col-sm-4">
-                                <div className="widget">
-                                    <h4 className="widget-title">Account</h4>
-                                    <ul className="links">
-                                        <li><a href="dashboard.html">My Account</a></li>
-                                        <li><a href="#">Track Your Order</a></li>
-                                        <li><a href="#">Payment Methods</a></li>
-                                        <li><a href="#">Shipping Guide</a></li>
-                                        <li><a href="#">FAQs</a></li>
-                                        <li><a href="#">Product Support</a></li>
-                                        <li><a href="#">Privacy</a></li>
-                                    </ul>
-                                </div>{/* End .widget */}
-                            </div>{/* End .col-lg-2 */}
-                            <div className="col-lg-3 col-sm-4">
-                                <div className="widget">
-                                    <h4 className="widget-title">About</h4>
-                                    <ul className="links">
-                                        <li><a href="about.html">About Porto</a></li>
-                                        <li><a href="#">Our Guarantees</a></li>
-                                        <li><a href="#">Terms And Conditions</a></li>
-                                        <li><a href="#">Privacy policy</a></li>
-                                        <li><a href="#">Return Policy</a></li>
-                                        <li><a href="#">Intellectual Property Claims</a></li>
-                                        <li><a href="#">Site Map</a></li>
-                                    </ul>
-                                </div>{/* End .widget */}
-                            </div>{/* End .col-lg-3 */}
-                            <div className="col-lg-2 col-sm-4">
-                                <div className="widget">
-                                    <h4 className="widget-title">Features</h4>
-                                    <ul className="links">
-                                        <li><a href="#">Powerful Admin Panel</a></li>
-                                        <li><a href="#">Mobile &amp; Retina Optimized</a></li>
-                                        <li><a href="#">Super Fast HTML Template</a></li>
-                                        <li><a href="#">1st Fully Working Ajax Theme</a></li>
-                                        <li><a href="#">33 Unique Shop Demos</a></li>
-                                    </ul>
-                                </div>{/* End .widget */}
-                            </div>{/* End .col-lg-2 */}
-                        </div>{/* End .row */}
-                    </div>{/* End .footer-middle */}
-                    <div className="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
-                        <p className="footer-copyright py-3 pr-4 mb-0">© Porto eCommerce. 2021. All Rights Reserved</p>
-                        <div className="social-icons py-3">
-                            <a href="#" className="social-icon social-facebook icon-facebook" target="_blank" title="Facebook" />
-                            <a href="#" className="social-icon social-twitter icon-twitter" target="_blank" title="Twitter" />
-                            <a href="#" className="social-icon social-linkedin fab fa-linkedin-in" target="_blank" title="Linkedin" />
-                        </div>{/* End .social-icons */}
-                    </div>{/* End .footer-bottom */}
-                </div>{/* End .container */}
-            </footer>{/* End .footer */}
-            {/* End .page-wrapper */}
-            {/* <div className="loading-overlay">
-                <div className="bounce-loader">
-                    <div className="bounce1" />
-                    <div className="bounce2" />
-                    <div className="bounce3" />
-                </div>
-            </div> */}
+           
+            <Footer/>
         </div>
     );
 }
