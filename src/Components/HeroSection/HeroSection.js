1b2f03364bec4fe2d28b947712467d60f1ceb5d5
nhussein2026 <naser738nas@gmail.com>
2022-11-15 20:55:12 +0300

    hero-section

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,3 +1,4 @@
+import './HeroSection.css'
 const HeroSection = () => {
 
     return (
@@ -7,11 +8,11 @@ const HeroSection = () => {
                 <div className="category-banner p-0">
                     <div className="container">
                         <div className="row align-items-center">
-                            <div className="col-md-5" style={{ backgroundImage: 'url(assets/images/cuted-logo.png)'}}>
+                            <div className="col-md-5" style={{ backgroundImage: 'url()'}}>
                                 <h2 className="m-b-2">Safha</h2>
                                 <h3 className="ls-n-10 font-weight-normal line-height-1 text-body m-0">Readers Are Leaders</h3>
                             </div>
-                            <div className="col-md-3 col-lg-4" style={{ backgroundImage: 'url(assets/images/demoes/demo25/banners/shop-banner.png)' }}>
+                            <div className="col-md-3 col-lg-4" id="for-logo">
                             </div>
                             <div className="col-md-4 col-lg-3 text-md-center">
                                 <a href="#" className="btn btn-quaternary ml-0">Explore Books <i className="fas fa-long-arrow-alt-right" /></a>
