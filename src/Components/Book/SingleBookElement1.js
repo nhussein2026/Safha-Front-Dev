ceec883dcba7b3e67eda5944ee90062924ba1965
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-16 09:03:09 -0500

    edit singlebook

--- src/Components/Book/SingleBookElement1.js
+++ src/Components/Book/SingleBookElement1.js
@@ -1,20 +1,20 @@
-const SingleBookElement1 = ({book}) => {
+const SingleBookElement1 = ({book,i}) => {
     return (
       <>
             <div class="col-6 col-sm-4 col-lg-3">
                 <div class="product-default inner-quickview inner-icon">
                     <figure>
                         <a href="demo25-product.html">
-                            <img src={book?.cover}
+                            <img src="https://m.media-amazon.com/images/I/513j4X3mFjL.jpg"
                                 width="217" height="217" alt="product"/>
                         </a>
                         <div class="label-group">
-                          <div class="product-label label-hot">HOT</div>
+                          {/* <div class="product-label label-hot">HOT</div> */}
                         </div>
                         <div class="btn-icon-group">
-                            <a href="demo25-product.html"
+                            {/* <a href="demo25-product.html"
                                 class="btn-icon btn-add-cart product-type-simple"><i
-                                    class="icon-shopping-cart"></i></a>
+                                    class="icon-shopping-cart"></i></a> */}
                         </div>
                         <a href="ajax/product-quick-view.html" class="btn-quickview"
                           title="Quick View">Quick
@@ -33,12 +33,12 @@ const SingleBookElement1 = ({book}) => {
                         </h3>
                         <div class="ratings-container">
                             <div class="product-ratings">
-                                <span class="ratings" style="width:80%"></span>
-                                <span class="tooltiptext tooltip-top"></span>
+                                {/* <span class="ratings" style="width:80%"></span>
+                                <span class="tooltiptext tooltip-top"></span> */}
                             </div>
                         </div>
                         <div class="price-box">
-                            <span class="product-price">$299.0</span>
+                            {/* <span class="product-price">$299.0</span> */}
                         </div>
                     </div>
                 </div>
