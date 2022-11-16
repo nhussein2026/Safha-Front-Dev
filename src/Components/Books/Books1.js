d7fcb29fc4f8a9e33a5effdcee755bb0f711464d
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-16 15:43:18 -0500

    edit Book1

--- src/Components/Books/Books1.js
+++ src/Components/Books/Books1.js
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
-import SingleBookElement from "../Book/SingleBookElement";
 import SingleBookElement1 from "../Book/SingleBookElement1";
 import Footer from "../Footer/Footer";
+import HeroSection from "../HeroSection/HeroSection";
 import Navbar from "../Navbar/Navbar";
 import SecondNavbar from "../SecondNavbar/SecondNavbar";
 import Sidebar from "../SideBar/SideBar";
+// <!-- Plugins CSS File -->
+//   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+//   <!-- Main CSS File -->
+//   <link rel="stylesheet" href="assets/css/demo25.min.css">
+//   <link rel="stylesheet" type="text/css" href="assets/vendor/fontawesome-free/css/all.min.css">
+//   <link rel="stylesheet" type="text/css" href="assets/vendor/simple-line-icons/css/simple-line-icons.min.css"></link>
 // import '../../assets/css/demo/demo25/demo25.css'
 // import '../../assets/css/demo25.min.css'
 
@@ -29,28 +36,36 @@ const Books1 = () => {
     console.log("books",books)
     return(
         <>
-            <Navbar/>
-            <SecondNavbar/>
-            {/* <SingleBookElement /> */}
-            <div class="bg-white">
-                <div class="container">
-                    <div class="row main-content">
-                        <div class="col-lg-9">
-                            <nav class="toolbox sticky-header" data-sticky-options="{'mobile': true}">
-                                <div class="row">
-                                    {
-                                        books.map((book, i) => {
-                                            return <SingleBookElement1 book={book} key={i} />
-                                        })
-                                    }
+            {/* <body className="boxed"> */}
+                <div className="page-wrapper">
+                    <Navbar/>
+                    <HeroSection />
+                    <SecondNavbar/>
+                    <main className="main">
+                        <div className="bg-white">
+                            <div className="container">
+                                <div className="row main-content">
+                                    <div className="col-lg-9">
+                                        <nav className="toolbox sticky-header" data-sticky-options="{'mobile': true}">
+                                            <div className="toolbox-left">
+                                                <Sidebar/>
+                                                <div className="row">
+                                                    {
+                                                        books.map((book, i) => {
+                                                            return <SingleBookElement1 book={book} key={i} />
+                                                        })
+                                                    }
+                                                </div>
+                                            </div>  
+                                        </nav>
+                                    </div>
                                 </div>
-                            </nav>
+                            </div>
                         </div>
-                    </div>
+                    </main>
+                    <Footer />
                 </div>
-            </div>
-            
-            <Footer />
+            {/* </body> */}
         </>
     )
 }
