25d0bf8a5eeaaa8d2f87998579b1a9c294bf510a
Manalei97 <manal.ye.tr@gmail.com>
2022-11-17 17:09:44 +0300

    edit sidebarwrapper

--- src/Components/SideBarWrapper/SideBarWrapper.js
+++ src/Components/SideBarWrapper/SideBarWrapper.js
@@ -1,181 +1,50 @@
-const SideBarWrapper = () => {
+// import Sidebar from "../SideBar/SideBar";
+import CategoriesSideBar from "../SideBar/CategoriesSideBar";
+import FavoriteBooks from "../SideBar/FavoriteBooks";
+
+
+
+const SideBarWrapper = ({ book }) => {
 
     return (
-        <aside className="sidebar-product col-lg-3 mobile-sidebar">
-            <div className="sidebar-wrapper">
-                <div className="widget widget-info">
-                    <h3 className="widget-title m-b-3">AUTHOR</h3>
-                    <div className="widget-body bg-gray">
-                        <h4 className="font2 text-dark line-height-1 m-b-1">Wendy Walker</h4>
-                        <p className="font2 font-weight-normal line-height-1 ls-0 text-uppercase">12
-                            Books</p>
-                        <a href="demo25-shop.html" className="d-inline-block text-uppercase">View All
-                            Books</a>
+        <>
+            <div>
+                <aside className="sidebar-product col-lg-3 mobile-sidebar">
+
+                    <div classname="sidebar-wrapper">
+                        <div className="widget widget-info">
+                            <h3 className="widget-title m-b-3">AUTHOR</h3>
+                            <div className="widget-body bg-gray">
+                                <h4 className="font2 text-dark line-height-1 m-b-1">Wendy Walker</h4>
+                                <p className="font2 font-weight-normal line-height-1 ls-0 text-uppercase">12
+                                    Books</p>
+                                <a href="#" className="d-inline-block text-uppercase">View All
+                                    Books</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="widget widget-featured">
-                    <h3 className="widget-title m-b-3">FEATURED BOOKS</h3>
-                    <div className="widget-body p-0">
-                        <div className="owl-carousel widget-featured-products">
-                            <div className="featured-col">
-                                <div className="product-default left-details product-widget">
-                                    <figure>
-                                        <a href="demo25-product.html">
-                                            <img src="assets/images/demoes/demo25/products/small/product-1.jpg" width={75} height={75} alt="product" />
-                                        </a>
-                                    </figure>
-                                    <div className="product-details">
-                                        <h3 className="product-title">
-                                            <a href="demo25-product.html">The Night Before</a>
-                                        </h3>
-                                        <div className="ratings-container">
-                                            <div className="product-ratings">
-                                                <span className="ratings" style={{ width: '80%' }} />
-                                                {/* End .ratings */}
-                                                <span className="tooltiptext tooltip-top" />
-                                            </div>{/* End .product-ratings */}
-                                        </div>{/* End .product-container */}
-                                        <div className="price-box">
-                                            <span className="product-price">$299.0</span>
-                                        </div>{/* End .price-box */}
-                                    </div>{/* End .product-details */}
-                                </div>
-                                <div className="product-default left-details product-widget">
-                                    <figure>
-                                        <a href="demo25-product.html">
-                                            <img src="assets/images/demoes/demo25/products/small/product-2.jpg" width={75} height={75} alt="product" />
-                                        </a>
-                                    </figure>
-                                    <div className="product-details">
-                                        <h3 className="product-title">
-                                            <a href="demo25-product.html">Call an Audible</a>
-                                        </h3>
-                                        <div className="ratings-container">
-                                            <div className="product-ratings">
-                                                <span className="ratings" style={{ width: '0%' }} />
-                                                {/* End .ratings */}
-                                                <span className="tooltiptext tooltip-top" />
-                                            </div>{/* End .product-ratings */}
-                                        </div>{/* End .product-container */}
-                                        <div className="price-box">
-                                            <span className="product-price">$299.0</span>
-                                        </div>{/* End .price-box */}
-                                    </div>{/* End .product-details */}
-                                </div>
-                                <div className="product-default left-details product-widget">
-                                    <figure>
-                                        <a href="demo25-product.html">
-                                            <img src="assets/images/demoes/demo25/products/small/product-3.jpg" width={75} height={75} alt="product" />
-                                        </a>
-                                    </figure>
-                                    <div className="product-details">
-                                        <h3 className="product-title">
-                                            <a href="demo25-product.html">I'll Never Tell</a>
-                                        </h3>
-                                        <div className="ratings-container">
-                                            <div className="product-ratings">
-                                                <span className="ratings" style={{ width: '0%' }} />
-                                                {/* End .ratings */}
-                                                <span className="tooltiptext tooltip-top" />
-                                            </div>{/* End .product-ratings */}
-                                        </div>{/* End .product-container */}
-                                        <div className="price-box">
-                                            <span className="old-price">$199.0</span>
-                                            <span className="product-price">$129.0</span>
-                                        </div>{/* End .price-box */}
-                                    </div>{/* End .product-details */}
-                                </div>
-                            </div>{/* End .featured-col */}
-                            <div className="featured-col">
-                                <div className="product-default left-details product-widget">
-                                    <figure>
-                                        <a href="demo25-product.html">
-                                            <img src="assets/images/demoes/demo25/products/small/product-1.jpg" width={75} height={75} alt="product" />
-                                        </a>
-                                    </figure>
-                                    <div className="product-details">
-                                        <h3 className="product-title">
-                                            <a href="demo25-product.html">The Night Before</a>
-                                        </h3>
-                                        <div className="ratings-container">
-                                            <div className="product-ratings">
-                                                <span className="ratings" style={{ width: '80%' }} />
-                                                {/* End .ratings */}
-                                                <span className="tooltiptext tooltip-top" />
-                                            </div>{/* End .product-ratings */}
-                                        </div>{/* End .product-container */}
-                                        <div className="price-box">
-                                            <span className="product-price">$299.0</span>
-                                        </div>{/* End .price-box */}
-                                    </div>{/* End .product-details */}
-                                </div>
-                                <div className="product-default left-details product-widget">
-                                    <figure>
-                                        <a href="demo25-product.html">
-                                            <img src="assets/images/demoes/demo25/products/small/product-2.jpg" width={75} height={75} alt="product" />
-                                        </a>
-                                    </figure>
-                                    <div className="product-details">
-                                        <h3 className="product-title">
-                                            <a href="demo25-product.html">Call an Audible</a>
-                                        </h3>
-                                        <div className="ratings-container">
-                                            <div className="product-ratings">
-                                                <span className="ratings" style={{ width: '0%' }} />
-                                                {/* End .ratings */}
-                                                <span className="tooltiptext tooltip-top" />
-                                            </div>{/* End .product-ratings */}
-                                        </div>{/* End .product-container */}
-                                        <div className="price-box">
-                                            <span className="product-price">$299.0</span>
-                                        </div>{/* End .price-box */}
-                                    </div>{/* End .product-details */}
-                                </div>
-                                <div className="product-default left-details product-widget">
-                                    <figure>
-                                        <a href="demo25-product.html">
-                                            <img src="assets/images/demoes/demo25/products/small/product-3.jpg" width={75} height={75} alt="product" />
-                                        </a>
-                                    </figure>
-                                    <div className="product-details">
-                                        <h3 className="product-title">
-                                            <a href="demo25-product.html">I'll Never Tell</a>
-                                        </h3>
-                                        <div className="ratings-container">
-                                            <div className="product-ratings">
-                                                <span className="ratings" style={{ width: '0%' }} />
-                                                {/* End .ratings */}
-                                                <span className="tooltiptext tooltip-top" />
-                                            </div>{/* End .product-ratings */}
-                                        </div>{/* End .product-container */}
-                                        <div className="price-box">
-                                            <span className="old-price">$199.0</span>
-                                            <span className="product-price">$129.0</span>
-                                        </div>{/* End .price-box */}
-                                    </div>{/* End .product-details */}
-                                </div>
-                            </div>{/* End .featured-col */}
-                        </div>{/* End .widget-featured-slider */}
-                    </div>{/* End .widget-body */}
-                </div>{/* End .widget */}
-                <div className="widget widget-custom">
-                    <h3 className="widget-title m-b-1">CUSTOM HTML BLOCK</h3>
-                    <div className="widget-body p-0">
-                        <h4 className="mb-2">This is a custom sub-title.</h4>
-                        <p className="font2 text-dark mb-0">Lorem ipsum dolor sit amet, consectetur elitad
-                            adipiscing Cras non placerat
-                            mi.</p>
-                    </div>
-                </div>
-                <div className="sidebar-banner d-flex justify-content-end" style={{ backgroundImage: 'url(assets/images/demoes/demo25/banners/banner-sidebar.jpg)', backgroundColor: '#343339' }}>
-                    <div className="banner-layer">
-                        <h3 className="line-height-1 text-uppercase text-white m-b-1">Award Winners</h3>
-                        <p className="font2 line-height-1 mb-2">12 Books</p>
-                        <a href="demo25-shop.html" className="btn btn-primary">View Collections<i className="fas fa-long-arrow-alt-right" /></a>
-                    </div>
-                </div>
-            </div>
-        </aside>
+                    {/* <Sidebar/> */}
+
+                    <div className="sidebar-wrapper">
+                        <CategoriesSideBar />
+                        <div className="widget widget-featured pb-0">
+                            <h3 className="widget-title">Books</h3>
+                            {
+                                book.map((book, i) => {
+                                    // console.log(i)
+                                    if (i < 2) {
+                                        return (<FavoriteBooks book={book} />)
+                                    }
+                                })
+                            }
+
+                        </div>{/* End .widget */}
+                    </div>{/* End .sidebar-wrapper */}
+
+                </aside>
+
+            </div >
+        </>
     )
 }
-export default SideBarWrapper ;
+export default SideBarWrapper;
