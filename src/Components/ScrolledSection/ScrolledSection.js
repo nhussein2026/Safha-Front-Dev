58a78b40f85ebdfd0e28b14feec2ce0754198cfc
nhussein2026 <naser738nas@gmail.com>
2022-11-18 20:47:53 +0300

    Fixed ScrollSection

--- src/Components/ScrolledSection/ScrolledSection.js
+++ src/Components/ScrolledSection/ScrolledSection.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactSwipe from 'react-swipe';
 import "./ScrolledSection.css";
+import categoryimg1 from "../../assets/images/categories/cover/scifi-fantasy.jpg"
+import bookImg from "../../assets/images/categories/product-5.jpg"
+
 
 const ScrolledSectioin = () => {
     let reactSwipeEl;
@@ -17,44 +20,140 @@ const ScrolledSectioin = () => {
                     swipeOptions={{ continuous: false }}
                     ref={el => (reactSwipeEl = el)}
                 >
-                    <div> <section className="scrollport">
-                        <div className="categories-cards">
-                            <h1>hi this is me the big</h1>
-                            <div className="big-dev">
-                                <h1>big one</h1>
+                    {/* 1st category */}
+                    <div>
+                        <div className='main'>
+                            <div className="intro-section">
+                                <div className="home-slider ">
+                                    <div className="home-slide">
+                                        <a href="/">
+                                            <img src={categoryimg1} alt="slider" id='category-img' />
+                                            <div className="home-slide-content text-center">
+                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </section></div>
-                    <div> <section className="scrollport">
-                        <div className="categories-cards">
-                            <h1>hi this is me the big</h1>
-                            <div className="big-dev">
-                                <h1>big one</h1>
+                    </div>
+                    {/*2nd category  */}
+                    <div>
+                        <div className='main'>
+                            <div className="intro-section">
+                                <div className="home-slider ">
+                                    <div className="home-slide">
+                                        <a href="/">
+                                            <img src={categoryimg1} alt="slider" id='category-img' />
+                                            <div className="home-slide-content text-center">
+                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </section></div>
-                    <div> <section className="scrollport">
-                        <div className="categories-cards">
-                            <h1>hi this is me the big</h1>
-                            <div className="big-dev">
-                                <h1>big one</h1>
+                    </div>
+                    {/* 3rd Category */}
+                    <div>
+                        <div className='main'>
+                            <div className="intro-section">
+                                <div className="home-slider ">
+                                    <div className="home-slide">
+                                        <a href="/">
+                                            <img src={categoryimg1} alt="slider" id='category-img' />
+                                            <div className="home-slide-content text-center">
+                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </section></div>
+                    </div>
+                    {/* 4th Category */}
+                    <div>
+                        <div className='main'>
+                            <div className="intro-section">
+                                <div className="home-slider ">
+                                    <div className="home-slide">
+                                        <a href="/">
+                                            <img src={categoryimg1} alt="slider" id='category-img' />
+                                            <div className="home-slide-content text-center">
+                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* 5th Category */}
+                    <div>
+                        <div className='main'>
+                            <div className="intro-section">
+                                <div className="home-slider ">
+                                    <div className="home-slide">
+                                        <a href="/">
+                                            <img src={categoryimg1} alt="slider" id='category-img' />
+                                            <div className="home-slide-content text-center">
+                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* 6th Category */}
+                    <div>
+                        <div className='main'>
+                            <div className="intro-section">
+                                <div className="home-slider ">
+                                    <div className="home-slide">
+                                        <a href="/">
+                                            <img src={categoryimg1} alt="slider" id='category-img' />
+                                            <div className="home-slide-content text-center">
+                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* 7th Category */}
+                    <div>
+                        <div className='main'>
+                            <div className="intro-section">
+                                <div className="home-slider ">
+                                    <div className="home-slide">
+                                        <a href="/">
+                                            <img src={categoryimg1} alt="slider" id='category-img' />
+                                            <div className="home-slide-content text-center">
+                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </ReactSwipe>
-                <button onClick={() => reactSwipeEl.next()}>Next</button>
-                <button onClick={() => reactSwipeEl.prev()}>Previous</button>
+                {/* <div> */}
+                    <button onClick={() => reactSwipeEl.next()}>Next</button>
+                    <button onClick={() => reactSwipeEl.prev()}>Previous</button>
+                {/* </div> */}
             </div>
 
 
-            {/* <section className="scrollport">
-            <div className="categories-cards">
-                <h1>hi this is me the big</h1>
-                <div className="big-dev">
-                 <h1>big one</h1>
-               </div>
-            </div>
-        </section> */}
+
+
+
+
+
+            {/* the new design### */}
+
 
         </>
     )
