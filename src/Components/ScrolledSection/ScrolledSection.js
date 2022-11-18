539ade395f44de79d91c7f41729f412ecd8971f5
nhussein2026 <naser738nas@gmail.com>
2022-11-18 21:41:52 +0300

    desiggn of scroll section

--- src/Components/ScrolledSection/ScrolledSection.js
+++ src/Components/ScrolledSection/ScrolledSection.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import ReactSwipe from 'react-swipe';
 import "./ScrolledSection.css";
 import categoryimg1 from "../../assets/images/categories/cover/scifi-fantasy.jpg"
+import literatureCover from "../../assets/images/categories/cover/literature-fiction.jpg";
+import artCover from "../../assets/images/categories/cover/arts-photography.jpg";
+import noval from "../../assets/images/categories/cover/business-investing.jpg";
+import polticsCover from "../../assets/images/categories/cover/mystery-suspense.jpg"
 import bookImg from "../../assets/images/categories/product-5.jpg"
 
 
@@ -17,21 +21,51 @@ const ScrolledSectioin = () => {
             <div>
                 <ReactSwipe
                     className="carousel"
-                    swipeOptions={{ continuous: false }}
+                    swipeOptions={{ continuous: true }}
                     ref={el => (reactSwipeEl = el)}
                 >
                     {/* 1st category */}
                     <div>
-                        <div className='main'>
-                            <div className="intro-section">
-                                <div className="home-slider ">
-                                    <div className="home-slide">
-                                        <a href="/">
-                                            <img src={categoryimg1} alt="slider" id='category-img' />
-                                            <div className="home-slide-content text-center">
-                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
-                                            </div>
-                                        </a>
+                        <div class="d-flex justify-content-between">
+                            <div className='main'>
+                                <div className="intro-section">
+                                    <div className="home-slider ">
+                                        <div className="home-slide">
+                                            <a href="/">
+                                                <img src={artCover} alt="slider" id='category-img' />
+                                                <div className="home-slide-content text-center">
+                                                    <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='main'>
+                                <div className="intro-section">
+                                    <div className="home-slider ">
+                                        <div className="home-slide">
+                                            <a href="/">
+                                                <img src={literatureCover} alt="slider" id='category-img' />
+                                                <div className="home-slide-content text-center">
+                                                    <h3 className="ls-n-10 text-white mb-0">Literature &amp; Fiction</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='main'>
+                                <div className="intro-section">
+                                    <div className="home-slider ">
+                                        <div className="home-slide">
+                                            <a href="/">
+                                                <img src={categoryimg1} alt="slider" id='category-img' />
+                                                <div className="home-slide-content text-center">
+                                                    <h3 className="ls-n-10 text-white mb-0">Education &amp; Science</h3>
+                                                </div>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -39,16 +73,46 @@ const ScrolledSectioin = () => {
                     </div>
                     {/*2nd category  */}
                     <div>
-                        <div className='main'>
-                            <div className="intro-section">
-                                <div className="home-slider ">
-                                    <div className="home-slide">
-                                        <a href="/">
-                                            <img src={categoryimg1} alt="slider" id='category-img' />
-                                            <div className="home-slide-content text-center">
-                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
-                                            </div>
-                                        </a>
+                        <div class="d-flex justify-content-between">
+                            <div className='main'>
+                                <div className="intro-section">
+                                    <div className="home-slider ">
+                                        <div className="home-slide">
+                                            <a href="/">
+                                                <img src={noval} alt="slider" id='category-img' />
+                                                <div className="home-slide-content text-center">
+                                                    <h3 className="ls-n-10 text-white mb-0">Novals</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='main'>
+                                <div className="intro-section">
+                                    <div className="home-slider ">
+                                        <div className="home-slide">
+                                            <a href="/">
+                                                <img src={polticsCover} alt="slider" id='category-img' />
+                                                <div className="home-slide-content text-center">
+                                                    <h3 className="ls-n-10 text-white mb-0">Politics</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='main'>
+                                <div className="intro-section">
+                                    <div className="home-slider ">
+                                        <div className="home-slide">
+                                            <a href="/">
+                                                <img src={categoryimg1} alt="slider" id='category-img' />
+                                                <div className="home-slide-content text-center">
+                                                    <h3 className="ls-n-10 text-white mb-0">Relegion</h3>
+                                                </div>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -61,77 +125,9 @@ const ScrolledSectioin = () => {
                                 <div className="home-slider ">
                                     <div className="home-slide">
                                         <a href="/">
-                                            <img src={categoryimg1} alt="slider" id='category-img' />
+                                            <img src={noval} alt="slider" id='category-img' />
                                             <div className="home-slide-content text-center">
-                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* 4th Category */}
-                    <div>
-                        <div className='main'>
-                            <div className="intro-section">
-                                <div className="home-slider ">
-                                    <div className="home-slide">
-                                        <a href="/">
-                                            <img src={categoryimg1} alt="slider" id='category-img' />
-                                            <div className="home-slide-content text-center">
-                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* 5th Category */}
-                    <div>
-                        <div className='main'>
-                            <div className="intro-section">
-                                <div className="home-slider ">
-                                    <div className="home-slide">
-                                        <a href="/">
-                                            <img src={categoryimg1} alt="slider" id='category-img' />
-                                            <div className="home-slide-content text-center">
-                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* 6th Category */}
-                    <div>
-                        <div className='main'>
-                            <div className="intro-section">
-                                <div className="home-slider ">
-                                    <div className="home-slide">
-                                        <a href="/">
-                                            <img src={categoryimg1} alt="slider" id='category-img' />
-                                            <div className="home-slide-content text-center">
-                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* 7th Category */}
-                    <div>
-                        <div className='main'>
-                            <div className="intro-section">
-                                <div className="home-slider ">
-                                    <div className="home-slide">
-                                        <a href="/">
-                                            <img src={categoryimg1} alt="slider" id='category-img' />
-                                            <div className="home-slide-content text-center">
-                                                <h3 className="ls-n-10 text-white mb-0">Arts &amp; Photography</h3>
+                                                <h3 className="ls-n-10 text-white mb-0">None Fiction</h3>
                                             </div>
                                         </a>
                                     </div>
@@ -141,8 +137,8 @@ const ScrolledSectioin = () => {
                     </div>
                 </ReactSwipe>
                 {/* <div> */}
-                    <button onClick={() => reactSwipeEl.next()}>Next</button>
-                    <button onClick={() => reactSwipeEl.prev()}>Previous</button>
+                <button onClick={() => reactSwipeEl.next()}>Next</button>
+                <button onClick={() => reactSwipeEl.prev()}>Previous</button>
                 {/* </div> */}
             </div>
 
