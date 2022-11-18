1bdf2f9ac7aa6cb287563b8a0b96471c01303d39
nhussein2026 <naser738nas@gmail.com>
2022-11-18 18:01:11 +0300

    fiex scrolled section

--- src/Components/ScrolledSection/ScrolledSection.js
+++ src/Components/ScrolledSection/ScrolledSection.js
@@ -1,24 +1,64 @@
-import Footer from "../Footer/Footer";
-import Navbar from "../Navbar/Navbar";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactSwipe from 'react-swipe';
 import "./ScrolledSection.css";
 
 const ScrolledSectioin = () => {
-
-    return(
+    let reactSwipeEl;
+    return (
         <>
-        <section className="scrollport">
+            {/* begining of the scroll part */}
+
+
+
+            <div>
+                <ReactSwipe
+                    className="carousel"
+                    swipeOptions={{ continuous: false }}
+                    ref={el => (reactSwipeEl = el)}
+                >
+                    <div> <section className="scrollport">
+                        <div className="categories-cards">
+                            <h1>hi this is me the big</h1>
+                            <div className="big-dev">
+                                <h1>big one</h1>
+                            </div>
+                        </div>
+                    </section></div>
+                    <div> <section className="scrollport">
+                        <div className="categories-cards">
+                            <h1>hi this is me the big</h1>
+                            <div className="big-dev">
+                                <h1>big one</h1>
+                            </div>
+                        </div>
+                    </section></div>
+                    <div> <section className="scrollport">
+                        <div className="categories-cards">
+                            <h1>hi this is me the big</h1>
+                            <div className="big-dev">
+                                <h1>big one</h1>
+                            </div>
+                        </div>
+                    </section></div>
+                </ReactSwipe>
+                <button onClick={() => reactSwipeEl.next()}>Next</button>
+                <button onClick={() => reactSwipeEl.prev()}>Previous</button>
+            </div>
+
+
+            {/* <section className="scrollport">
             <div className="categories-cards">
                 <h1>hi this is me the big</h1>
                 <div className="big-dev">
                  <h1>big one</h1>
                </div>
-                {/* <div className="small-dev"><h2>Small one</h2></div> */}
             </div>
-       
-        
-      </section>
+        </section> */}
 
         </>
     )
 }
+// ReactDOM.render(<Carousel />, document.getElementById('app'));
+
 export default ScrolledSectioin;
