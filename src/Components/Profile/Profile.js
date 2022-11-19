6fac30a586282ce16a0b88f7c0f218ae7bc05a24
nhussein2026 <naser738nas@gmail.com>
2022-11-19 16:00:47 +0300

    Add proflie photos

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,14 @@
+import './Profile.css'
+import backgroundImage from "../../assets/images/profile/56eaa2e41500002a000b2512.jpeg"
+
+
+
 
 const Profile = () => {
 
-    return(
+    return (
         <>
-         <a href="login.html">
+            {/* <a href="login.html">
             <div className="header-user">
                 <i className="icon-user-2"></i>
                 <div className="header-userinfo">
@@ -11,7 +16,24 @@ const Profile = () => {
                     <h4 className="mb-0">My Account</h4>
                 </div>
             </div>
-        </a>
+        </a> */}
+
+            <div className='container-fluid' id='grand-parent'>
+                <div className='row' id='rowDiv' >
+                    
+                    <div className='col-8' id='background-parent'>
+                            <img src={backgroundImage} id="backgroundImage" alt="Background Image" />
+                    </div>
+                    <div className='col-6 d-flex justify-content-center' id='child-img'>
+                            <img src={backgroundImage} id="personalImg" alt="Background Image" />
+                    </div>
+                    <div className='col-5'>
+
+                    </div>
+                </div>
+            </div>
+
+
         </>
     )
 }
