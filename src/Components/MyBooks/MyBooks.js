3f95b40c7335af5dc35d992350aec4a55837427c
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-19 18:56:51 -0500

    edit singleBookElement and MyBooks

--- src/Components/MyBooks/MyBooks.js
+++ src/Components/MyBooks/MyBooks.js
@@ -102,12 +102,9 @@ const MyBooks = () => {
                                             <div className="row">
                                                 {
                                                     addedBooks.map((book, i) => {
-                                                        return <SingleBookElement book={book} key={i} customize="MyBooks"/>
+                                                        return <SingleBookElement book={book} key={i} customize="MyFavorites"/>
                                                     })
                                                 }
-                                                {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
-                                                    <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
-                                                </svg> */}
                                                 <div className="col-6 col-sm-4 col-lg-3">
                                                     <div className="product-default inner-quickview inner-icon">
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="17rem" height="17rem" fill="currentColor" id="addIcon" className="bi bi-plus-lg" viewBox="0 0 16 16">
