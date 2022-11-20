144e631be7d3869b3f6dbc380054566cf62529d3
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-20 12:30:05 -0500

    edit categories

--- src/Components/Categories/CategoriesDesigne.js
+++ src/Components/Categories/CategoriesDesigne.js
@@ -1,3 +1,4 @@
+import HeroSection from "../HeroSection/HeroSection";
 import CategoriesSideBar from "../SideBar/CategoriesSideBar";
 import Categories from "./Categories";
 import './CategoriesDesigne.css'
@@ -6,15 +7,45 @@ const CategoriesDesigne = () => {
 
     return (
         <>
-            <h2 className="section-title pb-3 mb-3 col-12">Categories</h2>
+            {/* <h2 className="section-title pb-3 mb-3 col-12">Categories</h2>
 
-            <div className="d-fex">
+            <div className="d-fex"> */}
                 {/* <div className="side-cat"> */}
-                    <CategoriesSideBar />
+                    {/* <CategoriesSideBar /> */}
                 {/* </div>
                 <div className="categoryPlace"> */}
-                <Categories />
+                {/* <Categories /> */}
                 {/* </div> */}
+            {/* </div> */}
+
+            <div className="page-wrapper">
+                <HeroSection />
+                <main className="main">
+                    <div className="bg-white">
+                        <div className="container">
+                            <div className="row main-content">
+                                <div class="sidebar-overlay"></div>
+                                <aside className="sidebar-shop col-lg-3 order-lg-first mobile-sidebar">
+                                    <div className="sidebar-wrapper">
+                                        <CategoriesSideBar />
+                                    </div>
+                                </aside>
+                                <div className="col-lg-9">
+                                    <div className="col-6 col-sm-4 col-lg-6">
+                                        <ul class="nav nav-tabs" id="ulStyle">
+                                            <li class="nav-item">
+                                                <a class="nav-link active" id="active" aria-current="page" href="#">Categories</a>
+                                            </li> 
+                                        </ul>
+                                    </div>
+                                    <div className="row">
+                                        <Categories />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
             </div>
 
         </>
