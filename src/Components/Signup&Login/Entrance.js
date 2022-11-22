18baea422a0c05e1f1388c18c98389d252b0dad6
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-22 13:27:16 -0500

    edit entrance

--- src/Components/Signup&Login/Entrance.js
+++ src/Components/Signup&Login/Entrance.js
@@ -2,31 +2,34 @@
 import "./Entrance.css"
 import { AuthContext } from "../../contexts/Authcontext";
 import { useContext } from "react";
-import { Link } from "@mui/material";
+import { Link } from "react-router-dom";
 
 
 const Entrance = () => {
     const { loggedIn, signOut } = useContext(AuthContext)
     return(
         <>
-            <div className="img-avatar-entrance">
-                <Link to="/profile">
-                    <a href="#">
-                        <img src={window.localStorage.getItem('avatar')} />
-                    </a>
-                </Link>
-            </div>
+            
             {loggedIn?
-                <Link to="/signout">
-                    <a href="#">
-                        <button
-                            onClick={signOut}
-                            className="btn btn-primary w-49"
-                            id='signup-bttn'>SIGN OUT
-                        </button>
-                        {/* <button  id='signup-bttn' className="button">Signout</button> */}
-                    </a>
-                </Link>
+                <>
+                    <div className="img-avatar-entrance">
+                        <Link to="/profile">
+                            <a href="#">
+                                <img src={window.localStorage.getItem('avatar')} />
+                            </a>
+                        </Link>
+                    </div>
+                    <Link to="/signout">
+                        <a href="#">
+                            <button
+                                onClick={signOut}
+                                className="btn btn-primary w-49"
+                                id='signup-bttn'>SIGN OUT
+                            </button>
+                            {/* <button  id='signup-bttn' className="button">Signout</button> */}
+                        </a>
+                    </Link>
+                </>
                 : 
                  <Link to="/login">
                     <a href="#">
