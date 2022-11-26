b629033a96b92fbd5c36d831856c6498ae4daa5b
Ghadir Alselwi <melodieunique8484@gmail.com>
2022-11-26 17:53:32 -0500

    edit reviews and SigelReviews and activate like button

--- src/Components/Books/SingleBook/Reviews/Reviews.js
+++ src/Components/Books/SingleBook/Reviews/Reviews.js
@@ -4,38 +4,14 @@ import { AuthContext } from "../../../../contexts/Authcontext";
 import SingleReview from "./SingleReview/SingleReview";
 
 const Reviews = ({book}) => {
-    const { token } = useContext(AuthContext)
-    const content= useRef()
-    console.log("book?.id",book?.id);
-    
+        
     return (
         <>
-            {/* <div className="product-single-tabs font2"> */}
-                {/* <ul className="nav nav-tabs" role="tablist">
-                    <li className="nav-item">
-                        <a className="nav-link" id="product-tab-reviews" 
-                            data-toggle="tab" href="#" 
-                            role="tab" aria-controls="product-reviews-content" 
-                            aria-selected="false">
-                            Reviews</a>
-                    </li>
-                </ul> */}
-                {
-                    book?.Reviews?.map((review, i) => {
-                        return (<SingleReview review={review} />)
-                })
-                }
-                {/* <ul className="nav nav-tabs" role="tablist">
-                    <li className="nav-item">
-                        <a className="nav-link" id="product-tab-reviews" 
-                            data-toggle="tab" href="#" 
-                            role="tab" aria-controls="product-reviews-content" 
-                            aria-selected="false">
-                            Add A Review</a>
-                    </li>
-                </ul> */}
-            {/* </div> */}
-                
+            {
+                book?.Reviews?.map((review, i) => {
+                    return (<SingleReview review={review} />)
+            })
+            }   
         </>
     );
 }
